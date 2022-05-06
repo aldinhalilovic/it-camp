d48ad176fa4ccb304861c88773caffbc20fddedd
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-06 18:47:09 +0200

    java

---
+++ JS/UVOD/app.js
@@ -0,0 +1,23 @@
+console.log("Prvi ispis u JS konzoli")
+
+// dve kose crte-komentar
+
+'nesto' //string
+65 //number
+true,false //boolean
+["string", 75, true, ["string", 22]]
+
+null // ne postoji
+undefined // ima to nesto, ali se ne zna sta kako i slicno
+{}
+
+
+var ime = "Ime je:Isko"
+console.log(ime)
+ime = "Drugi isko"
+
+console.log(ime)
+
+console.log(2+2)
+console.log("2" + "2")
+console.log("ime"+ "prezime")
