862a3c6546a9d238d24a6e8c96d6fc70b6ad0d64
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-09 18:47:13 +0200

    javascript

---
+++ JS/drugi cas/app.js
@@ -0,0 +1,86 @@
+
+// LET i CONST;
+
+// const je konstantna varijabla, nema vise da se menja
+
+// const ime = "Max" 
+
+// ime = "Isko"     //ovo ne radi nista
+
+// let godine = 30
+
+// console.log ( ime + " " + godine )
+
+// godine = 24
+
+// console.log ( ime + " " + godine)
+
+
+// console.log ( 5===5 )    // uvek 3 put =, proverava jednakost leve i desne strane po tipu i vrednosti
+
+// console.log ( 5 !== 6)  // proverava razliku, da li je leva strana razlicita od desne
+
+// console.log ( 6>5 )     // uporedjivanje stvari, da li je manje vece/ manje ili jednaka / veca ili jednako
+
+// const ime = "aldin"
+// const godine = 40
+// const meseci = godine * 12
+// 
+// console.log (meseci)
+
+// const ime = prompt ("unesi ime") 
+// const godine = prompt ("unesi broj godina")
+
+// alert ("ziveo si" + godine * 12 + " meseci")
+
+// let age = 20;
+// console.log (++age) // ++ pre varijable dodaje za 1 pa onda printa, ++ posle varijable printa, pa za sledeci console log printa sabiranje
+
+// const age = "Nesto"
+
+// if (age >= 18) {
+//     console.log ("Punoletan si")
+// }
+
+// else if (age < 18) {
+//     console.log ("Nisi punoletan")
+// }
+
+// else {
+//     console.log("Izvrsava se samo u slucaju kada ni jedan uslov nije zadovoljen")
+// }
+
+
+// const age = prompt ("Unesi tvoje godine")
+
+// if (+age === 18) {
+//     console.log ("Tek si postao punoletan")
+// }
+// else if (+age > 18) {
+//     console.log("Ti si odrastao")
+// }
+// else if (+age > 12) {
+//     console.log("Ti si tinejdzer")
+// }
+
+// else {
+//     console.log ("Ti si dete")
+// }
+
+// const a = 4
+// const b = false
+
+// console.log( a && b)            && uslov ako su i levo i desno true , onda daje true, ostalo nista
+
+// const a = 5
+// const b = 4
+// const c = 0
+
+// console.log (a && b && c)  AND operator
+
+// const a = 4
+// const b = false;
+
+// console.log ( a || b)    || predstavlja proveru da li je bilo sta tacno, i print-a prvog tacnog
+
+// console.log (!4);             ! predstavlja negaciju, print-a suprotnost od true ili false  
