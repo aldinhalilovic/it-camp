2e6e435d8668e2c7f016b412871194b86b47ccce
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-13 18:30:59 +0200

    javascript

--- JS/Fizzbuzz/app.js
+++ JS/Fizzbuzz/app.js
@@ -11,26 +11,5 @@ for (let i=1; i<=100; i++ ) {
         console.log (i)
     }
 }
-``
-
-// for (let i=0; i<=100; i++ ) {
-//     if ( i % 3 === 0) {
-//         console.log ("FIZZ")
-//     } else if ( i % 5 === 0) {
-//         console.log ("BUZZ")
-//     } else {
-//         console.log (i)
-//     }
-//     for (let i = 1; i<=100; i ++) {
-//         if ( i % 3 === 0 || i % 5 === 0) {
-//             console.log ("FIZZ BUZZ")
-//         }
-//     }
-    
-//     // else {
-    //     console.log (i)
-    // }
 
 
-
-// console.log (i)
---
+++ JS/cetvrti cas/app.js
@@ -0,0 +1,35 @@
+// const rec = "Sta ste radili danas";
+// let counter = 0
+// for ( let i = 0; i < rec.length; i++) {
+//     if ( rec [i] === " ") {
+//         counter++
+//     }
+// }
+// console.log (counter)
+
+
+// const string = "ovo je string"
+
+// let newString = "";
+
+// for ( let i = 0; i < string.length; i++) {
+//     const element = string[i];
+
+//     if ( i === 0) {
+//         newString += element.toUpperCase();
+//     } else if (string [i - 1] === " "){
+//         newString += element.toUpperCase();
+//     } else {
+//         newString += element;
+//     }
+// }
+
+// console.log (newString);
+
+
+const string = "ovo je string"
+let newString = ""
+
+for ( let i = 0; i < string.length; i++ ) {
+    
+}
