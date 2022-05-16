596c64368429118d7d3c089b7d6a3681b37d2659
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-16 18:41:54 +0200

    switch/ternary/truthy

--- JS/cetvrti cas/app.js
+++ JS/cetvrti cas/app.js
@@ -1,35 +1,45 @@
-// const rec = "Sta ste radili danas";
-// let counter = 0
-// for ( let i = 0; i < rec.length; i++) {
-//     if ( rec [i] === " ") {
-//         counter++
-//     }
-// }
-// console.log (counter)
+let godine = 15
 
-
-// const string = "ovo je string"
-
-// let newString = "";
-
-// for ( let i = 0; i < string.length; i++) {
-//     const element = string[i];
-
-//     if ( i === 0) {
-//         newString += element.toUpperCase();
-//     } else if (string [i - 1] === " "){
-//         newString += element.toUpperCase();
-//     } else {
-//         newString += element;
-//     }
-// }
-
-// console.log (newString);
-
-
-const string = "ovo je string"
-let newString = ""
-
-for ( let i = 0; i < string.length; i++ ) {
-    
+switch(godine) {
+    case 20:
+        console.log ("Ima 20 godina")
+        break;
+    case 15:
+        console.log ("Ima 15 godina")
+        break;
+    default:
+        console.log ("Nije upao ni u jedan case")
 }
+
+const isSunny = true;
+//  if (isSunny) =  if (isSunny === true)
+//  if (!isSunny) = if (isSunny === false) || if (isSunny !=== true)
+if (isSunny ) {
+    console.log ("Ponesi naocare");
+} else {
+    console.log ("nemoj da nosis naocare");
+}
+
+//                ||
+// TERNARY OPEARATOR
+
+isSunny ? console.log ("Danas je suncano") : console.log ("Nije suncano")
+
+let age = 19
+// let punoletstvo = age >= 18 ? true  : false;
+
+
+if ( age >= 18) {
+    punoletstvo = true;
+} else {
+    punoletstvo = false
+}
+
+ 
+console.log ("Punoletan", punoletstvo)
+
+// falsy  values su : 0, -0, "", null, undefined, NaN
+
+
+// MDN TRUTHY VALUES / DA SE ISTTRAZI TRUTHY I FALSY VALUES
+
