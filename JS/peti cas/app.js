7564c7c44af87d27662cf804bc3a08a2e7803152
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-18 18:35:31 +0200

    javascript,niz

---
+++ JS/peti cas/app.js
@@ -0,0 +1,39 @@
+//  NIZOVI
+
+// const cars = ["BMW", "Audi", "VW", "MB", "Skoda"]
+//              0      1       2     3      4
+
+// console.log (cars [1]) 
+
+// const cars = [ "BMW", 4 , 97 , true, null , [ 1, 3 , 4]]
+
+// const cars =  ["BMW", "Audi", "VW", "MB", "Skoda"]
+
+// cars[0] = 3;
+// console.log (cars)
+
+// cars.length = 0;
+// console.log (cars)
+
+const cars =  ["BMW", 4, undefined, "MB", "Skoda", null, NaN, false, true,]
+
+// for (let i = 0; i < cars.length; i++){
+//     console.log (cars[i])
+// }
+
+
+// for (let i = 0; i < cars.length; i++){
+//     if( cars[i]) {
+//         console.log (cars[i])
+//     }
+// }
+
+// for (let car in cars) {
+//     console.log (cars[car])
+// }
+
+const studenti = ["Zika", "Janko", "Sasa"]
+
+studenti [8] = "Marko"
+
+console.log (studenti)
