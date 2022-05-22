811f7518608477194f2a3c1a37aa35f31d9fe782
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-22 15:08:48 +0200

    javascript,funkicije,dom

---
+++ JS/sesti cas/dom/app.js
@@ -0,0 +1,32 @@
+ 
+// function pomnozi (prviBr, drugiBr, treciBr)  {
+//     let prviBr = prompt ("Unesi prvi broj")
+//     let drugiBr = prompt ("Unesi drugi broj")
+//     let treciBr = prompt ("Unesi treci broj")
+//     return prviBr * drugiBr * treciBr
+// }
+
+// pomnozi (1, 3, 5)
+
+
+// function pomnozi (a,b,c) {
+//     a = prompt ("Unesi prvi broj")
+//     b = prompt ("Unesi drugi broj")
+//     c = prompt ("Unesi treci broj")
+//     return a*b*c
+// }
+
+// let proizovd = (pomnozi())
+// console.log (proizovd)
+
+function mnozenje () {
+    let proizovd = 0
+    for ( let i = 0; i<prompt ("unesi broj brojeva"); i++) {
+        prompt ("Unesi broj")
+        
+    }
+    return prozivod
+}
+
+mnozenje()
+
