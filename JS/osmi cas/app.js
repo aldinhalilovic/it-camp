9a97c5d9212beffb59461ab52c70e3d57d56cc3f
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-05-29 13:22:16 +0200

    javascript,filter/map

--- JS/osmi cas/app.js
+++ JS/osmi cas/app.js
@@ -55,3 +55,66 @@ const booleans = [true, false, true, false];
 const aaa = parni.concat(booleans).filter((value) => typeof value === "boolean");
 
 console.log(aaa) 
+
+// map() funkcija, namenjena za pravljenje novog niza uz dodatne izmene.
+
+// da se napravi novi niz koji sadrzi kvadrate elemenata postojeceg niza
+
+const brojevi1 = [2, 5, 10, 3, 4, 8];
+
+const square = brojevi1.map((value) => value * value)
+
+console.log (square)
+
+// napraviti niz koji sadrzi kvadratne korene postojeceg niza
+
+const root = square.map((value) => Math.sqrt(value))
+
+console.log (root)
+
+
+// objasnjenje funkcije:
+//  const root2 = square.map((value, index, array) => {
+    // console.log ("Prvi argument: ", value); //vrednost elementa
+//     console.log ("Drugi argument: ", index); // Index u nizu
+//     console.log ("Treci argument: ", array); // Ceo nizz
+//     return
+// });
+
+const brojevi2 = [2, 5, 3, 1, 10, 8, 7];
+
+// kvadrirati brojeve koji imaju index 0,3,5;
+const square2 = brojevi2.map((value, index) =>{
+    if (index === 0 || index === 3 || index ===5) {
+        return value * value;
+    } else {
+        return value;
+    }
+})
+
+console.log (square2);
+
+
+// Zadatak. Napraviti funkciju koja pravi niz koji ce iz postojeceg napraviti novi uz ispunjenje uslova:
+// value > 2 => value * 7
+// value >2 and value <8 => (value*4)/2
+// Ostale vrednosti da ne uzima u obzir
+// I na kraju treba novi niz vratiti sa godinama <10.
+
+
+const DATA11 = [1, 3, 4, 2, 7, 6];
+const godineVezba = (ages) => {
+    let newAge = ages.map((value) => {
+        if (value > 2 && value < 8) {
+            return (value * 4) / 2;
+        } else if (value > 2) {
+            return value * 7
+        }
+    })
+    
+let newAge2 = newAge.filter((value) => value <10);
+return newAge2;
+
+};
+
+console.log (godineVezba(DATA11));
