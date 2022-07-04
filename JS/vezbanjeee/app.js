d53d49d548bf91b301b11ae5a17ceac74fb16d5f
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-07-04 02:14:36 +0200

    vezbanje

--- JS/vezbanjeee/app.js
+++ JS/vezbanjeee/app.js
@@ -109,10 +109,74 @@ console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
 
 DA SE POGLEDA OVAJ PRIMER SA MDN */
 
-const brojevi = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// const brojevi = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-parni = brojevi.map((el) => el % 2 ===0);
+// parni = brojevi.map((el) => el % 2 ===0);
 
-console.log(parni);
+// console.log(parni);
 
+// const ja = {
+//     ime: "Aldin",
+//     prezime : "Halilovic",
+//     roditelji : {
+//         otac : "Fahrudin",
+//         majka : "Alma",
+//     }
+// };
+
+
+// console.log(ja.roditelji);
+ 
+
+// const students = [
+//     {name: "Faris", grade: 8, year: 2 },
+//     {name: "Isko", grade: 10, year: 4 },
+//     {name: "Aldin", grade: 8, year: 2 },
+//     {name: "Amina", grade: 9, year: 4 },
+//     {name: "Sabina", grade: 9, year: 3 },
+//     {name: "Denis", grade: 7, year: 4 },
+//     {name: "Senad", grade: 6, year: 1 },
+// ];
+
+// console.log(students);
+
+// function getAvgGrade(array) {
+//     const grades = array.map((el) => el.grade);
+//     console.log(grades);
+//     const mid = grades.reduce((prev,curr) => prev + curr) / grades.length;
+//     console.log(mid.toFixed(2));
+//     return +mid.toFixed(2);
+// };
+
+// getAvgGrade(students);
+
+
+// const nummerAray = [10, 20, 30, 40, 100, 400, 1000];
+
+// console.log(Math.max.apply(null, nummerAray));
+
+const movies = [
+    {title: "Movie1", rating: 8, budget: "230M"},
+    {title: "Movie2", rating: 5, budget: "30M"},
+    {title: "Movie3", rating: 9, budget: "530M"},
+    {title: "Movie4", rating: 4, budget: "200M"},
+    {title: "Movie5", rating: 9.5, budget: "120M"},
+];
+
+movies[1].gledao = "Ne";
+console.log(movies)
+
+const newMoviesArray = movies.map((movie) => {
+    return {
+        title: movie.title,
+        budget: movie.budget,
+        rankovi: movie.rating,
+    };
+});
+
+console.log(newMoviesArray)
+
+
+
+// ASYNC JAVASCRIPT DA SE PROVEZBA MALO;
 
